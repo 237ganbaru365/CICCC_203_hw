e9c699bf78644139d01ac094bfa819309cc1b691
237ganbaru365 <f.ganbaru365@gmail.com>
2021-09-23 21:59:59 -0700

    progress2
    progress2

--- 0923hw.js
+++ 0923hw.js
@@ -8,10 +8,9 @@
 // [5, 4, 3, 2, 1] => true
 // [1, 5, 2] => false
 
-var arr = [];
 
 function isSorted(arr) {
-
+  var arr = [39, 42];
   //_1 function for  ascending ---> lexical scope
   var ascArr = arr.sort( 
     function (a, b) {
@@ -31,12 +30,4 @@ function isSorted(arr) {
 
 }
 
-function isSorted();
-
-console.log(arr);
-console.log(arr = [42]);
-console.log(arr = [39, 42]);
-console.log(arr = [42, 39]);
-console.log(arr = [1, 2, 3, 4, 5]);
-console.log(arr = [5, 4, 3, 2, 1]);
-console.log(arr = [1, 5, 2]);
+console.log(isSorted());
